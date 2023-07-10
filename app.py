5e0d0bf97ad391d676be146c5c9686c97a072da7
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-07-10 23:57:05 +0530

    twilio interaction

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from twilio.twiml.messaging_response import Message, MessagingResponse
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "<p>Hello world!</p>"
+
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    response = MessagingResponse()
+    message = Message()
+
+    message.body("Hi")
+    response.append(message)
+
+    print(response)
+    return str(response)
+
+if __name__ == '__main__':
+    app.run()
