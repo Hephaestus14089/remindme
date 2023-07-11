29dfb2c8472576abb73de07afb35da65c77962b8
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-07-12 00:35:16 +0530

    enhance methods of Task class
    checking corner cases, deleting unneeded attributes

--- task.py
+++ task.py
@@ -7,17 +7,45 @@ class Task:
     def update_details_description(self, newVal):
         self.details['description'] = newVal
 
-    def update_details_time(self, timeTuple):
-        self.details['start_time'], self.details['end_time'] = timeTuple
-
     def update_details_start_time(self, newVal):
-        self.details['start_time'] = newVal
+        if newVal == "":
+            try:
+                del self.details['start_time']
+            except:
+                return
+        else:
+            self.details['start_time'] = newVal
 
     def update_details_end_time(self, newVal):
-        self.details['end_time'] = newVal
+        if newVal == "":
+            try:
+                del self.details['end_time']
+            except:
+                return
+        else:
+            self.details['end_time'] = newVal
+
+    def update_details_time(self, timeTuple):
+        st, et = timeTuple
+        self.update_details_start_time(st)
+        self.update_details_end_time(et)
+
+    def update_title(self, newVal):
+        self.title = newVal if newVal != "" else "no title"
 
     def update_remind_time(self, newVal):
-        self.remind_time = newVal
+        # this is just for testing
+        # according to the current plan,
+        # updating remind_time would require expensive
+        # queue operations too, like a re-insertion
+        # might be, creating and inserting a new Task object
+        # and deleting the current one
+        #
+        # checking the value should not require checking here
+        # as it would be checked and converted to a 'time' type
+        # before it is passed to this function
+        if newVal != "":
+            self.remind_time = newVal
 
     def print(self):
         print(f"title: {self.title}")
@@ -27,7 +55,20 @@ class Task:
             print(self.details)
 
 if __name__ == '__main__':
-    t1 = Task("2:30", "Title")
+    t1 = Task("2:30")
     t1.print()
+
+    t1.update_title("title_test")
+    t1.update_remind_time("3:30")
     t1.update_details_description("test description")
+    t1.update_details_time(("4:00PM", "6:00PM"))
+    t1.print()
+
+    t1.update_title("")
+    t1.update_details_start_time("5:00")
+    t1.update_details_end_time("")
+    t1.print()
+
+    t1.update_remind_time("")
+    t1.update_details_time(("", ""))
     t1.print()
