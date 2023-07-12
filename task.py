e42c2ba8a6b5f40de61a0fbcf3b4f516e788481a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-07-12 23:12:56 +0530

    add time string validation
    also the tests

--- task.py
+++ task.py
@@ -53,3 +53,46 @@ class Task:
 
         if self.details:
             print(self.details)
+
+    def validate_time(self):
+        # format of remind_time :-
+        # hrs[:]mins (24hr clock)
+        # hrs[:]mins[AM/PM] (12hr clock)
+        # hrs[:]mins [AM/PM] (12hr clock)
+        # hrs[h]mins[m] (current time + entered)
+        # hrs[h] mins[m] (current time + entered)
+        # mins (current time + entered)
+        # days[d] (along with h and m: + current time)
+
+        time_str = self.remind_time
+
+
+        if time_str == "" or time_str == "0":
+            raise ValueError("invalid time format")
+
+        time_str_lst = time_str.split()
+
+        if len(time_str_lst) > 3:
+            raise ValueError("invalid time format")
+
+        # time_dict can have the following attributes:-
+        #
+        # clock: 'h:m' format (if twelve_hr attr is not present, assume 24 hr)
+        # twelve_hr: AM/PM (must have clock attr)
+        # days: n days
+        #
+        # or
+        #
+        # days: n days
+        # hrs: n hrs
+        # mins: n mins
+        #
+        time_dict = {}
+
+        # loop through time_str_lst and create time_dict
+        for element in time_str_lst:
+            pass
+
+        # validate time_dict
+
+        return time_dict
--- test.py
+++ test.py
@@ -20,5 +20,29 @@ def test_task():
     t1.update_details_time(("", ""))
     t1.print()
 
+def test_time_str_validation():
+    rt_lst = ["5:15", "5:5AM", "6:00 PM", "2d 4h 3m", "1h5m", "45", "2d", "2d 4:00 PM", "1h 30m 3D"]
+    rt_bogus_lst = ["5:a7", "4:4 P M", "60:7", "1 a b 4", "", "0"]
+
+    print("Testing good list...")
+    for time_str in rt_lst:
+        print("time_str: " + time_str)
+        tsk = Task(time_str)
+        try:
+            tsk.validate_time()
+        except ValueError as err:
+            print("err: ", err)
+
+    print("Testing bad list...")
+    for time_str in rt_bogus_lst:
+        print("time_str: " + time_str)
+        tsk = Task(time_str)
+        try:
+            tsk.validate_time()
+        except ValueError as err:
+            print("err: ", err)
+
+
 if __name__ == '__main__':
-    test_task()
+    # test_task()
+    test_time_str_validation()
