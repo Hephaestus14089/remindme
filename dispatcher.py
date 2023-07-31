f1c5769f7f3abd80224b51259a44aaef944308ae
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-07-31 16:37:11 +0530

    plan Dispatcher class

--- dispatcher.py
+++ dispatcher.py
@@ -2,18 +2,33 @@ from dotenv import load_dotenv
 import os
 from twilio.rest import Client
 
-load_dotenv()
+class Dispatcher:
+    def __init__(self):
+        load_dotenv()
+        # fetch necessary environment variables
+        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        # create Twilio Client for this object
+        self.client = Client(account_sid, auth_token)
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-client = Client(account_sid, auth_token)
+    def dispatch_message(self, message_body):
+        self.client.messages.create(
+            from_=f"whatsapp:{os.getenv('TWILIO_PHONE_NUMBER')}",
+            body=f"{message_body}",
+            to=f"whatsapp:{os.getenv('RECEIVER_PHONE_NUMBER')}"
+        )
 
-message_body = "still testing..."
+    def dispatch_reminder(self, task_obj):
+        # accept a Task object
+        # create the reminder message
+        # dispatch the message
+        pass
 
-message = client.messages.create(
-  from_=f"whatsapp:{os.getenv('TWILIO_PHONE_NUMBER')}",
-  body=f"{message_body}",
-  to=f"whatsapp:{os.getenv('RECEIVER_PHONE_NUMBER')}"
-)
+    def dispatch_schedule(self, task_queue):
+        # accept a TaskQueue tuple
+        # create schedule message
+        # dispatch the message
+        pass
 
-print(message.sid)
+if __name__ == "__main__":
+    Dispatcher().dispatch_message("Test Dispatcher class.")
