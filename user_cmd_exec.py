ddfc208332ac7a97694be156db269737d916b206
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-08-03 21:43:47 +0530

    implement Executor operations
    all major operations of the Executor

--- user_cmd_exec.py
+++ user_cmd_exec.py
@@ -10,28 +10,77 @@ class Executor:
         self.task_q = task_queue
         self.dispatcher = Dispatcher()
 
-    def create_task(self):
-        # create Task object
-        # insert Task object in TaskQueue
-        pass
+    def display_error(self, err_msg="unknown error"):
+        self.dispatcher.dispatch_message("Error Occurred!\n" + err_msg)
+
+    def create_task(self, remind_time_str, title="no title"):
+        new_task = Task(remind_time_str, title)
+        self.task_q.insert(new_task)
 
     def remove_task(self, index):
-        # remove Task object at given index in TaskQueue
-        pass
+        if self.task_q.remove(index) == None:
+            self.display_error("the entered index in inaccessible!")
 
-    # update task remind_time
-    def update_remind_time(self, index, new_val):
-        pass
+    def update_remind_time(self, index, new_remind_time_str):
+        removed_task = self.remove_task(index)
+        if removed_task != None:
+            self.create_task(Task(new_remind_time_str, removed_task.title))
+        else:
+            self.display_error("the entered index in inaccessible!")
+
+    def update_title(self, index, new_val):
+        task = self.task_q.peek_at(index)
+        if task != None:
+            task.update_title(new_val)
+        else:
+            self.display_error("the entered index in inaccessible!")
 
     # update task details
     def update_details(self, index, attr, new_val):
-        # update the specified attribute of the Task details dictionary
-        # if attr == 'timing' invoke Task_obj.update_details_time
-        pass
+        if attr in ['description', 'start_time', 'end_time', 'timing', 'timings']:
+            task = self.task_q.peek_at(index)
+            if task != None:
+                if attr == 'timing' or attr == 'timings':
+                    # new_val must be a Tuple : (start_time, end_time)
+                    task.update_details_timings(new_val)
+                elif attr == 'description':
+                    task.update_details_description(new_val)
+                elif attr == 'start_time':
+                    task.update_details_start_time(new_val)
+                else:
+                    task.update_details_end_time(new_val)
+            else:
+                self.display_error("the entered index in inaccessible!")
+        else:
+            self.display_error("invalid attribute specified!")
 
-    def display_task_queue(self, index_needed):
-        # display all Tasks in the TaskQueue
-        pass
+    def display_task_queue(self, index_needed=True, details_needed=False, limit=0):
+        tasks_tuple = self.task_q.export_schedule_tuple()
+        if limit != 0:
+            tasks_tuple = tuple([tasks_tuple[i] for i in range(limit)])
+        self.dispatcher.dispatch_schedule(tasks_tuple, index_needed, details_needed)
+
+    def display_task_at(self, index, details_needed=False):
+        task = self.task_q.peek_at(index)
+        if task != None:
+            self.dispatcher.dispatch_message(task.export_task_str(details_needed))
+        else:
+            self.display_error("the entered index in inaccessible!")
+
+if __name__ == "__main__":
+    tq = TaskQueue()
+    ex = Executor(tq)
+
+    ex.create_task("9:59 PM", "T 1")
+    ex.create_task("9:57 PM", "T 2")
+    ex.create_task("9:51 PM", "T 3")
+    ex.create_task("9:54 PM", "T 4")
+
+    ex.display_task_queue()
+    ex.remove_task(1)
+    ex.display_task_at(1)
+    ex.display_task_queue()
+    ex.display_task_at(10)
 
 
 class Interpreter():
