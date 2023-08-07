a9f94a0871274aef989cfa4bd1ee1e4d16e6fc48
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-08-07 18:00:33 +0530

    possible user command formats
    information moved from Interpreter comments to a seperate .txt file

--- user_cmd_exec.py
+++ user_cmd_exec.py
@@ -85,42 +85,7 @@ if __name__ == "__main__":
 
 class Interpreter():
     # interprets the messages sent by the user
-    # invokes the executor
-    # to execute the desired operation
-    # and passes provides it with the required data
-    #
-    # Acceptible user instruction-message formats :-
-    # 1. Based on CREATE instruction
-    #   "create {title} {remind time}"
-    #   "create {remind time} {title}"
-    #   "{title} {remind time}"
-    #   "{remind time} {title}"
-    #
-    # 2. Based on DELETE/REMOVE instruction
-    #   "delete/remove {TaskQueue index}"
-    #   "delete/remove {title}" : all task objects with matching titles (exact string match) will be removed from the TaskQueue
-    #
-    # 3. Based on LIST/SHOW instruction
-    #   "list/show" : display task objects in TaskQueue with index
-    #   "list index" : display task objects in TaskQueue with index
-    #   "list noindex" : display task objects in TaskQueue without index
-    #   "list {n}" : display the first n task object in TaskQueue
-    #
-    # 4. Based on PEEK/SHOW instruction
-    #   "peek" : display first Task object in TaskQueue
-    #   "peek/show {TaskQueue index}"
-    #
-    # 5. BASED on UPDATE/MODIFY instruction
-    #   "update/modify title {TaskQueue index} {new value}"
-    #   "update/modify remindtime {TaskQueue index} {new value}"
-    #   "update/modify description {TaskQueue index} {new value}"
-    #   "update/modify timing/timings {TaskQueue index} {new value}"
-    #   "update/modify starttime {TaskQueue index} {new value} {new value}" : start_time and end_time respectively
-    #   "update/modify endtime {TaskQueue index} {new value}"
-    #
-    # Interpreter will accept the above mentioned formats and
-    # call the Executor to execute operations accordingly
-
+    # invokes the executor to execute the desired operation
     def __init__(self, executor):
         self.executor = executor
 
